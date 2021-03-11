e07eabd93513e37bf154df693c2c30e115c193cf
TAKAYUKI MIYOSHI <pico.pico.hammer@icloud.com>
2021-03-11 21:48:53 +0900

    ハンバーガーメニューだいたい完成。最終チェック前まで完了

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 //ハンバーガーメニュー
-$(function (){
-    $('.js-burger-ul').hide();
-    $('.js-burger-line').click(function (){
-        $('.js-burger-ul').toggle(300);
-    })
-})
+$(function () {
+    // SPメニュー
+    $('.js-toggle-sp-menu').on('click', function () {
+        $(this).toggleClass('active');
+        $('.js-toggle-sp-menu-target').toggleClass('active');
+    });
+});
